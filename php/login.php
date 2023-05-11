1c0c0425448c837cc48cd5b3baa4ecde58271685
albertescobar <albert.escobar@bit-data.es>
2023-05-11 12:47:05 +0200

    validació usuario i contrasenya bbdd

--- php/login.php
+++ php/login.php
@@ -9,15 +9,19 @@ if (isset($_POST['login'])) {
 $email = $_POST['email'];
 $password_user = $_POST['password'];
 
-$query = $connection->prepare("SELECT * FROM usuario WHERE email='$email' /*AND contraseña='$password_user'*/");
+$query = $connection->prepare("SELECT * FROM usuario WHERE email='$email' AND password=$password_user");
 $query->execute();
 $result = $query->fetch(PDO::FETCH_ASSOC);
 
-$nombre = $result['nombre'];
+$nombre_bbdd = $result['nombre'];
+$email_bbdd = $result['email'];
+$password_bbdd = $result["password"];
 
-echo "hola ".$nombre . " " . $email . " " . $password;
+if($email_bbdd==$email && $password_user==$password_bbdd){
+echo "hola ";
+}else{
+  echo "error login ";
+}//end else
 
-
-
-}
+}//end if isset
 ?>
